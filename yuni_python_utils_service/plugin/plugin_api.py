4ec53cc81fbbb0e1f6e0aca2c89c5d3ad6fac13e
liyuier <me@yuier.com>
2024-12-25 01:28:10 +0800

    合并到 master

--- yuni_python_utils_service/plugin/plugin_api.py
+++ yuni_python_utils_service/plugin/plugin_api.py
@@ -1,8 +1,10 @@
 from ninja import NinjaAPI
 
+from yuni_python_utils_service.schema.schema import ResponseSchema
+
 plugin_api = NinjaAPI()
 
 
 @plugin_api.get("/list")
-def list_pic(request):
-    return "pic list"
+def list_pic(request) -> ResponseSchema:
+    pass
