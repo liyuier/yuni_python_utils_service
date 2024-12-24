5451e73c2e494cd0c6bb283826294c2638fee8c3
sagiri <sagiri@yui.com>
2024-12-25 01:28:10 +0800

    合并到 master

--- yuni_python_utils_service/api.py
+++ yuni_python_utils_service/api.py
@@ -1,11 +1,11 @@
 from ninja import NinjaAPI
 
+from .schema.schema import ResponseSchema
 from .service.hello import hello_service
 
 api = NinjaAPI()
 
 
 @api.get("/hello")
-def hello(request):
-    # return hello_service()
+def hello(request) -> ResponseSchema:
     return hello_service()
