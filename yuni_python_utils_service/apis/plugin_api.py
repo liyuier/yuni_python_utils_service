9d9029957b0b56a39ae454704201d421c90477d0
sagiri <sagiri@yui.com>
2025-01-02 01:18:38 +0800

    插件详情查看功能

--- yuni_python_utils_service/apis/plugin_api.py
+++ yuni_python_utils_service/apis/plugin_api.py
@@ -1,8 +1,8 @@
 from ninja import NinjaAPI
 
 from yuni_python_utils_service.schema.schema import ResponseSchema
-from ..service.plugin_service import draw_plugins_info
-from ..schema.plugin_schema import GetPluginsPicInfoSchema
+from ..service.plugin_service import draw_plugins_info, draw_plugin_detail
+from ..schema.plugin_schema import GetPluginsPicInfoSchema, GetPluginDetailInfoSchema
 
 plugin_api = NinjaAPI()
 
@@ -12,3 +12,8 @@ responseSchema = ResponseSchema()
 @plugin_api.post("/list")
 def list_pic(request, schema: GetPluginsPicInfoSchema) -> ResponseSchema:
     return responseSchema.ok(draw_plugins_info(schema))
+
+
+@plugin_api.post("/detail")
+def detail_pic(request, schema: GetPluginDetailInfoSchema) -> ResponseSchema:
+    return responseSchema.ok(draw_plugin_detail(schema))
